375b271429b2c3f83997321df59cac4a733b54df
PedroMendes <pedroeliasxexeu@gmail.com>
2026-04-20 12:04:18 -0300

    Configure PostgreSQL for production deployment on Render

--- back/Crud/Program.cs
+++ back/Crud/Program.cs
@@ -7,25 +7,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddScoped<Contatos.src.API.Services.ContatoService>();
 
-
+// Use PostgreSQL em produção, SQL Server em desenvolvimento/docker
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    var env = builder.Environment;
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    
+    if (env.IsProduction())
+    {
+        options.UseNpgsql(connectionString);
+    }
+    else
+    {
+        options.UseSqlServer(connectionString);
+    }
+});
 
 var app = builder.Build();
-  // Para expor o Swagger em todos os ambientes, deixe assim:
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    
-    // Caso queira restringir o Swagger apenas para desenvolvimento, basta descomentar abaixo:
-    
-    // if (app.Environment.IsDevelopment())
-    // {
-    //     app.UseSwagger();
-    //     app.UseSwaggerUI();
-    // }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
