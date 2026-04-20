47041d40a3779af0f4d45bd14e91b07f3f1d7fb6
PedroMendes <pedroeliasxexeu@gmail.com>
2026-04-20 14:41:13 -0300

    fix(Program): Add Postgres

--- back/Crud/Program.cs
+++ back/Crud/Program.cs
@@ -1,10 +1,11 @@
 using CrudContatos.src.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers(); 
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<Contatos.src.API.Services.ContatoService>();
@@ -25,10 +26,26 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     }
 });
 
-var app = builder.Build();
 
+// Banco qlServer, usando a conecttionString
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+// Para expor o Swagger em todos os ambientes, deixe assim:
 app.UseSwagger();
 app.UseSwaggerUI();
+
+
+// Restringir o Swagger apenas para desenvolvimento, basta descomentar abaixo:
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger()
+//     app.UseSwaggerUI();
+// }
 
 app.UseAuthorization();
 app.MapControllers();
