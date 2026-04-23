aa6e1d0b536a019a84a2929bbf67e210f06275a5
PedroMendes <pedroeliasxexeu@gmail.com>
2026-04-22 23:23:33 -0300

    chore(infra): reorganize docker and nginx configuration

--- Presetation/Project/vite.config.ts
+++ Presetation/Project/vite.config.ts
@@ -6,10 +6,9 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/Contatos': {
-        target: 'https://localhost:8081',
+      '/api': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
